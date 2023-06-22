53a168a58b9ea7aa3ac6e10f7e381f2ed079aeaa
sarahcssiqueira <sarahcso@hotmail.com>
2023-06-22 16:54:59 -0300

    style: update header and menu

--- footer.php
+++ footer.php
@@ -5,14 +5,14 @@
  * Contains the closing of the #content div and all content after.
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
  */
 ?><footer class="footer">
   <span>
-      Dentist Clinic Theme by: 
-      <a href="https://sarahjobs.com/">Sarah Siqueira.</a> 
+      Dentist Clinic Theme by 
+      <?php echo ('&nbsp') ?>
+      <a href="https://sarahjobs.com/">Sarah Siqueira</a> 
   </span>
-  <span class="copyright">© <?php  echo date( "Y" )?> All rights reserved.</span>
+  <span class="copyright">© 2021 - <?php  echo date("Y")?> All rights reserved.</span>
 </footer>
 <?php wp_footer();?>
 </body>
--- header.php
+++ header.php
@@ -14,41 +14,34 @@
     <title><?php echo get_bloginfo('name'); ?></title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet">
     <?php wp_head();?>
 </head>
 
 <body> 
   <header class="header">
 
-          <div class="header-title">
-          <?php
-    if (function_exists('the_custom_logo') ) {
-        the_custom_logo();
-    }
-    ?>
-              <a href="<?php echo get_home_url(); ?>">
-                  <?php echo get_bloginfo('name'); ?>
-              </a>
-          </div>
+        <div class="header-title">
+          
+            <a href="<?php echo get_home_url(); ?>">
+            <?php if (function_exists('the_custom_logo') ) {
+                    the_custom_logo();
+            }
+            ?>
+            </a>
+        </div>
 
-          <div class="header-menu">
-              <div class="header-menu-links">
-                <?php wp_nav_menu(
-                    array(
-                            'menu'=>'primary',
-                            'container'=>'',
-                            'theme_location'=>'primary',
-                            'items_wrap'=>'<li>%3$s</li>'
-                              )
-                ); 
-?>
-              </div>
-          </div>
+        <div class="header-menu">
+            <?php wp_nav_menu(
+                array(
+                    'menu'=>'primary',
+                    'container'=>'',
+                    'theme_location'=>'primary',
+                    'items_wrap'=>'<li>%3$s</li>'
+                    )
+            );?>
+        </div>
               
-          <div class="header-menu-mobile">
-              <i class="fa-solid fa-bars"></i>
-          </div>
+        <div class="header-menu-mobile">
+            <i class="fa-solid fa-bars"></i>
+        </div>
   </header>
