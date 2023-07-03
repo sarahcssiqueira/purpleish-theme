fa90a0ee9376d23a092708c4e1d31880b89c7aca
sarahcssiqueira <sarahcso@hotmail.com>
2023-07-03 15:08:36 -0300

    fix: .phpcs.xml
    refactor: fixed wpcs violations

--- template-parts/section-services.php
+++ template-parts/section-services.php
@@ -2,7 +2,6 @@
 /**
  * Template part for displaying services section
  *
- *
  * @link https://codex.wordpress.org/Template_Hierarchy
  */
 
@@ -22,10 +21,10 @@
 			<ul class="services">
 
 			<?php
-				$args = array(
+				$args = [
 					'post_type'      => 'services',
 					'posts_per_page' => -1,
-				);
+				];
 
 
 					$post_data = new WP_Query( $args );
